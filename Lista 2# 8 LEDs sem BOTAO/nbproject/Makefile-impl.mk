31ceb8d6cbf5ef29719f257ebb6f738768900140
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2024-10-22 15:30:30 -0300

    update

--- Lista 2# 8 LEDs sem BOTAO/nbproject/Makefile-impl.mk
+++ Lista 2# 8 LEDs sem BOTAO/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Lista 2#
+PROJECTNAME=Lista 2# 8 LEDs sem BOTAO
 
 # Active Configuration
 DEFAULTCONF=default
--- Lista 2# 8 LEDs sem BOTAO/nbproject/Makefile-variables.mk
+++ Lista 2# 8 LEDs sem BOTAO/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Lista_2_.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Lista_2_.production.hex
+CND_ARTIFACT_NAME_default=Lista_2__8_LEDs_sem_BOTAO.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lista_2__8_LEDs_sem_BOTAO.production.hex
