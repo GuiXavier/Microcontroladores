da592f2dea9344780755fe35fed4e1a134560bb0
Yan Danilo Lima Morejon <114151447+rasputinThepreacher@users.noreply.github.com>
2024-11-11 00:25:03 -0300

    update

--- Lista 4 primeiro exercicio/nbproject/Makefile-variables.mk
+++ Lista 4 primeiro exercicio/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Exemplo_pisca_led.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Exemplo_pisca_led.production.hex
+CND_ARTIFACT_NAME_default=Lista_4_primeiro_exercicio.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lista_4_primeiro_exercicio.production.hex
