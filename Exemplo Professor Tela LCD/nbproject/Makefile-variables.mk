1efd24838c53d901c6e03e2ac6000b1ad930104d
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2024-11-18 14:39:42 -0300

    update

---
+++ Exemplo Professor Tela LCD/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Exemplo_Professor_Tela_LCD.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Exemplo_Professor_Tela_LCD.production.hex
