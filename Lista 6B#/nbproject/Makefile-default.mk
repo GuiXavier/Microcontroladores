3e9914b4034c90d63da09b954f09b9ca87fef250
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2024-12-03 14:17:16 -0300

    update

--- Lista 6B#/nbproject/Makefile-default.mk
+++ Lista 6B#/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=codigo.c
+SOURCEFILES_QUOTED_IF_SPACED=codigo.c atividade6.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/codigo.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/codigo.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/codigo.p1 ${OBJECTDIR}/atividade6.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/codigo.p1.d ${OBJECTDIR}/atividade6.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/codigo.p1
+OBJECTFILES=${OBJECTDIR}/codigo.p1 ${OBJECTDIR}/atividade6.p1
 
 # Source Files
-SOURCEFILES=codigo.c
+SOURCEFILES=codigo.c atividade6.c
 
 
 
@@ -96,6 +96,14 @@ ${OBJECTDIR}/codigo.p1: codigo.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/codigo.d ${OBJECTDIR}/codigo.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/codigo.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/atividade6.p1: atividade6.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/atividade6.p1.d 
+	@${RM} ${OBJECTDIR}/atividade6.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/atividade6.p1 atividade6.c 
+	@-${MV} ${OBJECTDIR}/atividade6.d ${OBJECTDIR}/atividade6.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/atividade6.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/codigo.p1: codigo.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
@@ -104,6 +112,14 @@ ${OBJECTDIR}/codigo.p1: codigo.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/codigo.p1 codigo.c 
 	@-${MV} ${OBJECTDIR}/codigo.d ${OBJECTDIR}/codigo.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/codigo.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/atividade6.p1: atividade6.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/atividade6.p1.d 
+	@${RM} ${OBJECTDIR}/atividade6.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/atividade6.p1 atividade6.c 
+	@-${MV} ${OBJECTDIR}/atividade6.d ${OBJECTDIR}/atividade6.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/atividade6.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
--- Lista 7#/nbproject/Makefile-default.mk
+++ Lista 7#/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=atividade7.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/atividade7.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/atividade7.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/atividade7.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=atividade7.c
 
 
 
@@ -88,7 +88,23 @@ MP_PROCESSOR_OPTION=16F877
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/atividade7.p1: atividade7.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/atividade7.p1.d 
+	@${RM} ${OBJECTDIR}/atividade7.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/atividade7.p1 atividade7.c 
+	@-${MV} ${OBJECTDIR}/atividade7.d ${OBJECTDIR}/atividade7.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/atividade7.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/atividade7.p1: atividade7.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/atividade7.p1.d 
+	@${RM} ${OBJECTDIR}/atividade7.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mno-default-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/atividade7.p1 atividade7.c 
+	@-${MV} ${OBJECTDIR}/atividade7.d ${OBJECTDIR}/atividade7.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/atividade7.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
